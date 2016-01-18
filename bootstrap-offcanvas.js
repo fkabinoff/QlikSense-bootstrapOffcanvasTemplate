1c7baa7e8bf67a4b109b2f4ab9f39e956da460f6
Francis Kabinoff <francis.kabinoff@gmail.com>
2016-01-18 11:25:56 -0500

    Initial commit

---
+++ bootstrap-offcanvas.js
@@ -0,0 +1,36 @@
+/*global require*/
+/*
+ *    Fill in host and port for Qlik engine
+ */
+var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
+
+var config = {
+	host: window.location.hostname,
+	prefix: prefix,
+	port: window.location.port,
+	isSecure: window.location.protocol === "https:"
+};
+//to avoid errors in workbench: you can remove this when you have added an app
+var app;
+require.config({
+	baseUrl: (config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "" ) + config.prefix + "resources"
+});
+
+require( ["js/qlik"], function ( qlik ){
+	qlik.setOnError( function ( error ){
+		alert( error.message );
+	});
+
+	$(document).ready(function() {
+	  $('[data-toggle=offcanvas]').click(function() {
+	    $('.row-offcanvas').toggleClass('active');
+	  });
+	});
+
+	//callbacks -- inserted here --
+	//open apps -- inserted here --
+	
+	//get objects -- inserted here --
+	//create cubes and lists -- inserted here --
+
+});
